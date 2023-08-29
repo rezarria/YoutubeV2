7a7e2ef668b9497d6593a02d8235940235ac0b5e
rezarria <votanamcb@outlook.com>
2023-08-29 23:16:22 +0700

    thêm icon upload

--- src/navigator/tabs/TabNavigator.tsx
+++ src/navigator/tabs/TabNavigator.tsx
@@ -1,6 +1,8 @@
+import { Image } from 'native-base'
 import HomeTabScreen from './HomeTabScreen'
 import UploadTabScreen from './UploadTabScreen'
 import { Tab } from './type'
+import HomeLogo from '@assets/svg/home.svg'
 
 export default function TabNavigator() {
 	return (
@@ -11,11 +13,21 @@ export default function TabNavigator() {
 			<Tab.Screen
 				name={'Home'}
 				component={HomeTabScreen}
+				options={{ tabBarIcon: HomeLG }}
 			/>
 			<Tab.Screen
 				name={'Upload'}
 				component={UploadTabScreen}
+				options={{ tabBarIcon: UploadLG }}
 			/>
 		</Tab.Navigator>
 	)
 }
+
+const HomeLG = () => <HomeLogo />
+const UploadLG = () => (
+	<Image
+		source={require('@assets/img/upload.png')}
+		alt={'upload icon'}
+	/>
+)
